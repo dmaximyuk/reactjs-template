6478239dd6e8707adb69c0deca624cef7cfe423d
Dmitry <66935827+dmaximyuk@users.noreply.github.com>
2023-06-02 21:39:34 +0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { version } from "./package.json";
+import { compilerOptions } from "./tsconfig.json";
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-});
+  publicDir: "public",
+  build: {
+    outDir: path.join(compilerOptions.outDir, "web", version || "0.0.0"),
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[hash].js",
+        entryFileNames: "js/[hash].js",
+        assetFileNames: (opt) => {
+          // @ts-ignore
+          const [[, ext]] = Array.from(opt.name.matchAll(/.([0-9-a-z]+)$/g));
+          return `${ext}/[hash].${ext}`;
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsconfigPaths(),
+    react(),
+  ]
+})
