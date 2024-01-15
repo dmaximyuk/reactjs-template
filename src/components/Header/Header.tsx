8ff1b63d9b8915f71360c08c8b0a28d104e51957
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-01-15 18:31:29 +0500

    Update template

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,8 +16,6 @@ export const Header: FC<HeaderProps> = () => {
           lang: "ru",
         }}
       >
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={t("app.description")} />
         <title>{t("app.name")}</title>
       </Helmet>
