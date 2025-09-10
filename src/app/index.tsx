cadb03d5787e2901b4bae28acd24f1d8cd717dd1
Dmitry Maximyuk <d_maksimyk@vk.com>
2025-09-10 20:15:55 +0500

    chore: refactoring

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import "./app/styles/main.sass";
+import "@/app/styles/main.sass";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { RouterProvider, QueryClientProvider } from "@/app/providers";
 
-import "./shared/i18n";
+import "@/shared/i18n";
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
